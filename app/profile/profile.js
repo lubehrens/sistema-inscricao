cf3163ad512b7d620f2fe7819fe48590ab8f4948
Luisa Palmeira <lubpalmeira@gmail.com>
2015-03-02 12:30:48 -0300

    criacao do controlador do profile

---
+++ app/profile/profile.js
@@ -0,0 +1,38 @@
+enejApp.controller("profileController", function()
+{
+	this.user = {};
+	this.user.nomeUsuario;
+	this.user.email;
+	this.user.senha;
+	this.user.nome;
+	this.user.sobrenome;
+	this.user.nomeCracha;
+	this.user.cpf;
+	this.user.rg;
+	this.user.sexo;
+	this.user.cep;
+	this.user.pais;
+	this.user.estado;
+	this.user.cidade;
+	this.user.bairro;
+	this.user.endereco;
+	this.user.numero;
+	this.user.complemento;
+	this.user.nascimento;
+	this.user.universidade;
+	this.user.curso;
+	this.user.periodo;
+	this.user.federacao;
+	this.user.ej;
+	this.user.cargo;
+	this.user.camisa;
+	this.user.pne;
+	this.user.necessidades;
+	this.user.dieta;
+	this.user.alergia;
+
+	this.enviarForm = function()
+	{
+		myFirebaseRef.set(this.user);
+	}
+});
