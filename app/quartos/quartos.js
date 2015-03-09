5bd1115d9e5e4bb6c660a86f5d6ba141dbedadce
Luisa Palmeira <lubpalmeira@gmail.com>
2015-03-09 02:39:17 -0300

    CRUD de quartos (sem combobox de hotel)

---
+++ app/quartos/quartos.js
@@ -0,0 +1,58 @@
+angular.module('enejApp')
+
+.config(function($routeProvider) {
+	$routeProvider.when('/quartos', {
+		templateUrl: 'quartos/list.html',
+		controller: 'quartoListController',
+		controllerAs: 'ctrl'
+	}).when('/quartos/add', {
+		templateUrl: 'quartos/form.html',
+		controller: 'quartoFormController',
+		controllerAs: 'ctrl'
+	}).when('/quartos/edit/:id', {
+		templateUrl: 'quartos/form.html',
+		controller: 'quartoFormController',
+		controllerAs: 'ctrl'
+	})
+})
+
+.controller('quartoListController', function($http) {
+	var self = this;
+
+	self.quartos = {};
+
+	$http.get('https://amber-fire-952.firebaseio.com/quartos.json').success(function (data) {
+		self.quartos = data;
+	});
+
+	self.removerQuarto = function(key) {
+		$http.delete('https://amber-fire-952.firebaseio.com/quartos/'+key+'.json').success(function() {
+			delete self.quartos[key];
+		})
+	}
+})
+
+.controller('quartoFormController', function($http, $location, $routeParams) {
+	var self = this;
+
+	self.quarto = {};
+
+	if($routeParams.id) {
+		$http.get('https://amber-fire-952.firebaseio.com/quartos/'+$routeParams.id+'.json').success(function(data) {
+			self.quarto = data;
+		});
+	}
+
+	self.sendForm = function() {
+		if($routeParams.id) {
+			$http.put('https://amber-fire-952.firebaseio.com/quartos/'+$routeParams.id+'.json', self.quarto).success(function() {
+				$location.path('/quartos');
+			});
+		}
+		else {
+			$http.post('https://amber-fire-952.firebaseio.com/quartos.json', self.quarto).success(function() {
+				$location.path('/quartos');
+			});
+		}
+	}
+});
